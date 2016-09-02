94c400fe28d9ef0e84b3d50a2d4676e76f1fa79e
ghy <1724286863@qq.com>
2016-09-02 17:52:25 +0800

    掌握隐式启动，掌握使用Intent拨打电话。 1.隐式启动作用 2.权限的基本使用方法 3.拨打电话基本方法

---
+++ app/src/main/java/com/example/date_type/AnotherActivity.java
@@ -0,0 +1,24 @@
+package com.example.date_type;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.btnCall);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+}
